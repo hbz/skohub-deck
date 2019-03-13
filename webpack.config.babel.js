9d8517a658aa9476c23fba2c24c2adebf4be612b
Daniel Doblado <danieldoblado@gmail.com>
2019-03-13 17:02:14 +0100

    Check webpack mode and set env

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,20 +1,27 @@
 import path from 'path'
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
+module.exports = (env, options) => {
+  const { mode } = options
+  process.env.NODE_ENV = mode === 'production' ? mode : 'development'
+
+  const config = {
+    mode,
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
         }
-      }
-    ]
+      ]
+    }
   }
+  return config
 }
