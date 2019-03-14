5e7df54f54f1edd147374c5bdaf95704865a8ee2
Daniel Doblado <danieldoblado@gmail.com>
2019-03-14 15:56:08 +0100

    Add colors file

---
+++ src/styles/colors.js
@@ -0,0 +1,16 @@
+import { css } from '@emotion/core'
+
+export const primary = css`
+  color: #ff575d;
+`
+export const base = css`
+  color: #eff0f2;
+`
+
+export const secondary = css`
+  color: #222428;
+`
+
+export const accent = css`
+  color: #55555a;
+`
