f1dd6d094f26900d705f3a224a8260b5c50a0061
Daniel Doblado <danieldoblado@gmail.com>
2019-03-20 16:40:21 +0100

    Add test for HubURL component

---
+++ test/HubURL.test.js
@@ -0,0 +1,25 @@
+/* global expect */
+/* global describe */
+/* global test */
+
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+import WS from 'jest-websocket-mock'
+
+import HubURL from '../src/components/HubURL'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+const fakeURL = 'ws://localhost:8080'
+const server = new WS(fakeURL)
+
+describe('HubURL', () => {
+  test('Connects to fake WS', async () => {
+    shallow(<HubURL hubURL={fakeURL} removeURL={() => {}}/>)
+    await server.connected
+    await expect(server).toReceiveMessage('Hello Server from client!')
+    await server.send('Test')
+  })
+})
