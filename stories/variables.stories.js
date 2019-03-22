eb5768b4ed9ac2b2aa40253ef291b4a2b95b3488
Daniel Doblado <danieldoblado@gmail.com>
2019-03-22 16:01:11 +0100

    Add border to show the colors correctly

--- stories/variables.stories.js
+++ stories/variables.stories.js
@@ -7,7 +7,12 @@ storiesOf('Variables', module)
   .add('Colors', () => (
     Object.keys(colors).map(name => (
       <div key="name">
-        {name}: {colors[name]} <div style={{ backgroundColor: colors[name], width: '25px', height: '25px', display: 'inline-block' }}></div>
+        {name}({colors[name]}) <div style={{
+          backgroundColor: colors[name],
+          width: '25px',
+          height: '25px',
+          border: '1px solid black',
+          display: 'inline-block' }}></div>
       </div>
     ))
   ))
